e0ca712584e30dbd81e61ad886aad19a7bef942b
Ryu613 <411379453@qq.com>
2024-08-06 20:51:06 +0800

    opengl module

--- components/bites/src/static_plugin_loader.cpp
+++ components/bites/src/static_plugin_loader.cpp
@@ -4,8 +4,14 @@ namespace PrimalDawnBites {
     void StaticPluginLoader::load() {
         using namespace PrimalDawn;
         PrimalDawn::Plugin* plugin = NULL;
+#ifdef RENDERSYSTEM_OPENGL4
+        plugin = new GL4Plugin();
+        plugins.push_back(plugin);
+#endif
+#ifdef RENDERSYSTEM_VULKAN
         plugin = new VulkanPlugin();
         plugins.push_back(plugin);
+#endif
         Engine& engine = Engine::Instance();
         for (auto& p : plugins) {
             engine.installPlugin(p);
