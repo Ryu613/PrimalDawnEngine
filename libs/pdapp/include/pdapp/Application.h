df0e9a59219da0f9d9048030bc7875421b3c3156
Ryu613 <411379453@qq.com>
2024-12-27 19:29:14 +0800

    added samples and pdapp libs

---
+++ libs/pdapp/include/pdapp/Application.h
@@ -0,0 +1,57 @@
+#ifndef DAWN_PRIMALDAWN_PDAPP_APPLICATION_H
+#define DAWN_PRIMALDAWN_PDAPP_APPLICATION_H
+
+#include <string>
+
+namespace pd {
+    class Window;
+
+    struct AppConfig {
+
+    };
+    /**
+    * @brief 应用，所有渲染实例的主抽象
+    */
+    class Application {
+    public:
+        static Application& get();
+        virtual ~Application() = default;
+        /**
+        * @brief 应用执行前的准备工作
+        */
+        virtual bool prepare(const AppConfig& options);
+        
+        /**
+        * @brief 运行一次
+        * @param ms 距离上次经过的毫秒数
+        */
+        virtual void runOneFrame(float ms);
+        /**
+        * @brief 应用结束后的工作，比如清理资源
+        */
+        virtual void finish();
+
+        inline bool shouldClose() const {
+            return mClosed;
+        }
+
+        inline void close() {
+            mClosed = true;
+        }
+
+        Application(const Application& app) = delete;
+        Application(const Application&& app) = delete;
+        Application& operator=(const Application& app) = delete;
+        Application& operator=(Application& app) = delete;
+    protected:
+        float mFps{ 0.f };
+        float mFrameTime{ 0.f };
+        Window* mWindow{ nullptr };
+    private:
+        Application();
+        std::string mName{};
+        bool mClosed{ false };
+    };
+} // namespace pd
+
+#endif
