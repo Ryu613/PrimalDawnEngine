bfee6949b864a6dce7cb481561ba1ea33983327f
Ryu613 <411379453@qq.com>
2025-03-13 21:33:01 +0800

    update vk common

--- engine/src/platform/rs/vulkan/VulkanCommon.hpp
+++ engine/src/platform/rs/vulkan/VulkanCommon.hpp
@@ -3,7 +3,9 @@
 #define NOMINMAX
 // 防止vulkan的全局函数定义产生冲突
 #define VK_NO_PROTOTYPES
+#ifdef _WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
+#endif
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #include <vulkan/vulkan.hpp>
 // vulkan memory allocator
